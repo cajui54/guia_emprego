f4cc589406ba8e40011d3664a18c53ccd4e82834
Jackson Silva <cajui54@gmail.com>
2023-01-15 17:20:05 -0300

    has been weather section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import AppStyled from "./appStyled";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Weather from "./components/Weather/Weather";
 
 function App() {
   return (
     <AppStyled>
       <BrowserRouter>
         <Header/>
+          <Weather/>
           <Routes>
             <Route path='/' element={<Home/>}/>
           </Routes>
